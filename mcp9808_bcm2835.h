5b226de388bb9f1a4b5133430458512298570e6e
Brent <kb1lqd@gmail.com>
2020-04-24 21:50:35 -0700

    Added resolution definitions.

--- mcp9808_bcm2835.h
+++ mcp9808_bcm2835.h
@@ -1,3 +1,4 @@
+// Register definitions
 #define REG_RFU 0x00
 #define REG_CONFIG 0x01
 #define REG_ALERT_TUPPER 0x02
@@ -7,6 +8,12 @@
 #define REG_MANUFACTURER_ID 0X06
 #define REG_DEVICE_ID 0x07 
 #define REG_RESOLUTION 0X08
+
+// Resolution mode definitions
+#define RESOLUTION_0C5 0x00
+#define RESOLUTION_0C25 0x01
+#define RESOLUTION_0C125 0x02
+#define RESOLUTION_0C0625 0x03
 
 int mcp9808_read_reg(int reg_addr, char* buf , int len);
 int mcp9808_write_reg(int reg_addr, char databyte);
